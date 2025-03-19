a28a74a90681d6208cc8d4257c96eb05319c03e1
alex frith <alexfrith@alexs-MacBook-Pro-2.local>
2025-03-19 23:27:24 +1000

    fix site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,12 @@
-export const baseUrl = 'https://dataloop.com';
-export const sitemap = {
-  '/': {
-    changefreq: 'daily',
-    priority: 0.7,
-  }};
+export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
+export default async function sitemap() {
+  
 
+  let routes = [''].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes]
+}
